79e9bd8fdec86d40865bbe88ca3cdcbc54f2435c
Saurabh <tripathisaurabh0276@gmail.com>
2021-04-06 19:10:33 +0530

    variable added

--- src/main/java/com/ait/pbm/PhoneBookApplication.java
+++ src/main/java/com/ait/pbm/PhoneBookApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PhoneBookApplication {
+	private int var = 10;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhoneBookApplication.class, args);
