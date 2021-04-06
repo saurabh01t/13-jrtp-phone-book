7d80ead594f438bb11baeaa91d0af7b63fb2be4a
Saurabh <tripathisaurabh0276@gmail.com>
2021-04-06 21:04:00 +0530

    var3 added in dev

--- src/main/java/com/ait/pbm/PhoneBookApplication.java
+++ src/main/java/com/ait/pbm/PhoneBookApplication.java
@@ -8,7 +8,7 @@ public class PhoneBookApplication {
 
 	private int var = 10;
 	private int var1 = 15;
-	
+	private int var3 = 30;
 	public static void main(String[] args) {
 		SpringApplication.run(PhoneBookApplication.class, args);
 	}
